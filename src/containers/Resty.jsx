28c8c36e211fcb280bf479ba0f4a09f4bd64830a
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-08-02 17:37:08 -0700

    added history function and localStorage

---
+++ src/containers/Resty.jsx
@@ -0,0 +1,75 @@
+import { exists } from 'fs';
+import React, { Component } from 'react';
+import Form from '../components/form/Form';
+import HistoryList from '../components/history/HistoryList';
+import Payload from '../components/payload/Payload';
+import  { fetchApi } from '../services/resty.js';
+
+export default class Resty extends Component {
+  state = {
+    search: '',
+    method: '',
+    body: '',
+    payload: '',
+    history: [],
+  }
+
+  componentDidMount() {
+    const exists = localStorage.getItem('HISTORY');
+    exists
+      ? (this.setState({ history: JSON.parse(exists)
+      }))
+      : (localStorage.setItem('HISTORY', '[]'));
+  }
+
+  async fetchApi() {
+    const { search, method, body } = this.state;
+
+    const payload = await fetchApi(search, method, body);
+    this.setState({ payload });
+  }
+
+  getAndSetLocalStorage() {
+    const { search, method, body } = this.state;
+    const recent = { search, method, body };
+
+    const parsedHistory = JSON.parse(localStorage.getItem('HISTORY'));
+    parsedHistory.push(recent);
+
+    const newHistory = JSON.stringify(parsedHistory);
+    localStorage.setItem('HISTORY', newHistory);
+
+    this.setState({ history: parsedHistory });
+
+    handleChange = ({ target }) => {
+      this.setState({  [target.name]: target.value });
+    }
+
+    handleSubmit = async (e) => {
+      e.preventDefault();
+
+      this.fetchApi();
+
+      this.getAndSetLocalStorage();
+    }
+
+    render() {
+      const { search, method, body, payload, history } = this.state;
+
+      return (
+        <>
+          <Form
+            search={search}
+            method={method}
+            body={body}
+            onChange={this.handleChange}
+            onSubmit={this.handleSubmit}
+          />
+          <Payload
+            payload={payload}
+          />  
+          </>  
+      );
+    }
+  }
+}
